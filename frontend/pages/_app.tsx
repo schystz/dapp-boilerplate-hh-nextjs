082de8a955198008a4f1e64ee6def3a5956e2202
James Russell Orola <jrmorola@gmail.com>
2021-10-10 15:10:19 +0800

    Integrate useDapp hooks

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,20 @@
 import 'tailwindcss/tailwind.css'
 import type { AppProps } from 'next/app'
+import { ChainId, Config, DAppProvider } from '@usedapp/core'
+
+const config: Config = {
+  readOnlyChainId: ChainId.Mainnet,
+  readOnlyUrls: {
+    [ChainId.Mainnet]:
+      'https://mainnet.infura.io/v3/62687d1a985d4508b2b7a24827551934'
+  }
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <DAppProvider config={config}>
+      <Component {...pageProps} />
+    </DAppProvider>
+  )
 }
 export default MyApp
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,24 @@
 import type { NextPage } from 'next'
+import { useEthers } from '@usedapp/core'
 
 const Home: NextPage = () => {
+  const { activateBrowserWallet, account } = useEthers()
+
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-xl">dApp Boilertplate</h1>
+      <div className="py-4">
+        {account ? (
+          <p>Account: {account}</p>
+        ) : (
+          <button
+            className="bg-blue-400 text-white rounded py-2 px-4"
+            onClick={() => activateBrowserWallet()}
+          >
+            Connect
+          </button>
+        )}
+      </div>
     </div>
   )
 }
